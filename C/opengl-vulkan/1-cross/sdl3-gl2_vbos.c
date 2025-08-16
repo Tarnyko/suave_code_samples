93ee180661dac97f13aa399b11bf32e1a39cf210
Manuel Bachmann <tarnyko@tarnyko.net>
2025-08-16 10:27:22 +0200

    C: initial "opengl-vulkan/1-cross" samples
    Add OpenGL 1.1, 2.1, ES 2.0, ES 3.0 samples.

---
+++ C/opengl-vulkan/1-cross/sdl3-gl2_vbos.c
@@ -0,0 +1,127 @@
+/*
+* sdl3-gl2_vbos.c
+* Copyright (C) 2025  Manuel Bachmann <tarnyko.tarnyko.net>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//  Compile with:
+// gcc -std=c11 ... `pkg-config --cflags --libs sdl3 gl`
+
+#include <SDL3/SDL.h>
+#define GL_GLEXT_PROTOTYPES    // for OpenGL>1.1 imports
+#include <SDL3/SDL_opengl.h>
+
+
+#define LINES       2
+#define INIT_WIDTH  800
+#define INIT_HEIGHT 600
+
+
+static unsigned int _width  = INIT_WIDTH;
+static unsigned int _height = INIT_HEIGHT;
+
+ /* WORKING AREA : 
+  * [ -1.0;+1.0    +1.0;+1.0 ]
+  * [ -1.0;-1.0    +1.0;-1.0 ]
+  */
+static const GLfloat vertex_arr[LINES * 4] = {   // 1 line: 2 points * 2 coordinates ([-1.0f;1.0f])
+    -0.8f,  0.8f,    0.8f, -0.8f,                // - line 1 (\)
+    -0.8f, -0.8f,    0.8f,  0.8f,                // - line 2 (/)
+};
+
+static const GLubyte color_arr[LINES * 8] = {    // 1 line: 2 points * 4 colors ([R,G,B,A])
+    255, 0,   0, 255,      0, 255,   0, 255,     // - color 1 (Red->Green)
+      0, 0, 255, 255,    255, 255, 255, 255,     // - color 2 (Blue->White)
+};
+
+static const GLuint index_arr[LINES * 2] = {     // 1 line: 2 points ON 2 selected colors
+    0, 1,
+    2, 3,
+};
+
+
+
+void redraw(SDL_Window* window, GLuint* vbos)
+{
+    glViewport(0, 0, _width, _height);                         // size
+
+    glClearColor(0, 0, 0, 255);                                // background (Black)
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
+    glVertexPointer(2, GL_FLOAT, 0, 0);                        // 2 lines
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[1]);
+    glColorPointer(4, GL_UNSIGNED_BYTE, 0, 0);                 // 4 colors
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbos[2]);
+    glDrawElements(GL_LINES, 4, GL_UNSIGNED_INT, 0);           // match 4 points on 4 colors
+
+    SDL_GL_SwapWindow(window);
+}
+
+
+int main (int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    SDL_Window* window = SDL_CreateWindow(argv[0], INIT_WIDTH, INIT_HEIGHT, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    SDL_SetWindowResizable(window, true);
+
+    glEnableClientState(GL_VERTEX_ARRAY);    // to use "glVertexPointer()"
+    glEnableClientState(GL_COLOR_ARRAY);     // to use "glColorPointer()"
+
+    GLuint vbos[3];
+    glGenBuffers(3, vbos);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_arr), vertex_arr, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(color_arr), color_arr, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbos[2]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_arr), index_arr, GL_STATIC_DRAW);
+
+    while (true) {
+        SDL_Event e;
+
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_EVENT_QUIT  : { goto end; }
+                case SDL_EVENT_KEY_UP: { if (e.key.scancode == SDL_SCANCODE_ESCAPE) {
+                                           goto end; }
+                                       }
+                case SDL_EVENT_WINDOW_RESIZED: { _width  = e.window.data1;
+                                                 _height = e.window.data2; }
+            }
+        }
+
+        redraw(window, vbos);
+    }
+
+  end:
+    glDeleteBuffers(3, vbos);
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
---
+++ C/opengl-vulkan/1-cross/sdl3-gles3.c
@@ -0,0 +1,150 @@
+/*
+* sdl3-gles3.c
+* Copyright (C) 2025  Manuel Bachmann <tarnyko.tarnyko.net>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//  Compile with:
+// gcc -std=c11 ... `pkg-config --cflags --libs sdl3 glesv2`
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengles2.h>
+
+
+#define LINES       2
+#define INIT_WIDTH  800
+#define INIT_HEIGHT 600
+
+
+static unsigned int _width  = INIT_WIDTH;
+static unsigned int _height = INIT_HEIGHT;
+
+ /* WORKING AREA : 
+  * [ -1.0;+1.0    +1.0;+1.0 ]
+  * [ -1.0;-1.0    +1.0;-1.0 ]
+  */
+static const GLfloat vertex_arr[LINES * 4] = {   // 1 line: 2 points * 2 coordinates ([-1.0f;1.0f])
+    -0.8f,  0.8f,    0.8f, -0.8f,                // - line 1 (\)
+    -0.8f, -0.8f,    0.8f,  0.8f,                // - line 2 (/)
+};
+
+static const GLubyte color_arr[LINES * 8] = {    // 1 line: 2 points * 4 colors ([R,G,B,A])
+    255, 0,   0, 255,      0, 255,   0, 255,     // - color 1 (Red->Green)
+      0, 0, 255, 255,    255, 255, 255, 255,     // - color 2 (Blue->White)
+};
+
+static const GLuint index_arr[LINES * 2] = {     // 1 line: 2 points ON 2 selected colors
+    0, 1,
+    2, 3,
+};
+
+static const GLchar *vertex_shader =
+    "#version 300 es                        \n"
+    "                                       \n"
+    "layout(location=0) in vec4 p_position; \n" // 1st attribute: ID 0
+    "layout(location=1) in vec4 p_color;    \n" // 2nd attribute: ID 1
+    "out vec4 v_color;                      \n"
+    "                                       \n"
+    "void main()                            \n"
+    "{                                      \n"
+    "  v_color = p_color;                   \n"
+    "  gl_Position = p_position;            \n"  // (builtin)
+    "}                                      \n";
+
+static const GLchar *color_shader =
+    "#version 300 es                  \n"
+    "precision mediump float;         \n"
+    "                                 \n"
+    "in vec4 v_color;                 \n"
+    "out vec4 frag_color;             \n"
+    "                                 \n"
+    "void main()                      \n"
+    "{                                \n"
+    "  frag_color = v_color;          \n"
+    "}                                \n";
+
+
+
+void redraw(SDL_Window* window)
+{
+    glViewport(0, 0, _width, _height);                         // size
+
+    glClearColor(0, 0, 0, 255);                                // background (Black)
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // ID 0: 'p_position' in shader
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, vertex_arr);        // 2 lines
+    glEnableVertexAttribArray(0);
+
+    // ID 1: 'p_color' in shader (GL_TRUE to normalize: 0 -> 0.0f, 255 -> 1.0f)
+    glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, 0, color_arr); // 4 colors
+    glEnableVertexAttribArray(1);
+    
+    glDrawElements(GL_LINES, 4, GL_UNSIGNED_INT, index_arr);  // 4 points on 4 colors
+
+    SDL_GL_SwapWindow(window);
+}
+
+
+int main (int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(argv[0], INIT_WIDTH, INIT_HEIGHT, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    SDL_SetWindowResizable(window, true);
+
+    GLuint vert_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert_shader, 1, &vertex_shader, NULL);
+    glCompileShader(vert_shader);
+
+    GLuint frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, &color_shader, NULL);
+    glCompileShader(frag_shader);
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vert_shader);
+    glAttachShader(program, frag_shader);
+    glLinkProgram(program);
+    glUseProgram(program);
+
+    while (true) {
+        SDL_Event e;
+
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_EVENT_QUIT  : { goto end; }
+                case SDL_EVENT_KEY_UP: { if (e.key.scancode == SDL_SCANCODE_ESCAPE) {
+                                           goto end; }
+                                       }
+                case SDL_EVENT_WINDOW_RESIZED: { _width  = e.window.data1;
+                                                 _height = e.window.data2; }
+            }
+        }
+
+        redraw(window);
+    }
+
+  end:
+    glDeleteProgram(program);
+    glDeleteShader(frag_shader);
+    glDeleteShader(vert_shader);
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
