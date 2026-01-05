d269dfdd2ebbee5d3c1fb2e4e866efa924532871
Manuel Bachmann <tarnyko@tarnyko.net>
2026-01-05 19:04:47 +0100

    Rust: linux/landlock
    Example of using the Linux Landlock security API
    (similar to OpenBSD's "pledge()"):
    https://docs.kernel.org/userspace-api/landlock.html
    https://landlock.io/rust-landlock

---
+++ Rust/linux/landlock/src/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::error::Error;
+
+use landlock::{
+    Ruleset, RulesetAttr, RulesetCreatedAttr,
+    AccessFs, path_beneath_rules
+};
+
+
+
+fn main() -> Result<(), Box<dyn Error>>
+{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {0} <filename>\n", &args[0]);
+        return Ok(());
+    }
+
+
+    #[cfg(feature = "landlock")]
+    {
+        let abi = landlock::ABI::V6;
+
+        let Ok(rules) = Ruleset::default().handle_access(AccessFs::from_read(abi)) else {
+            return Err("Could not retrieve default Landlock rules, exiting...".into()); };
+
+        let Ok(mut path_rules) = rules.create() else {
+            return Err("Could not create custom Landlock rules, exiting...".into()); };
+
+        let path_rules_ref = &mut path_rules;
+        let Ok(_) = path_rules_ref.add_rules(path_beneath_rules(
+                                         vec![OsStr::new("/tmp")],
+                                         AccessFs::from_read(abi))
+                                    ) else {
+            return Err("Could not add Landlock path rule, exiting...".into()); };
+
+        let Ok(_) = path_rules.restrict_self() else {
+            return Err("Could not enforce Landlock policy, exiting...".into()); };
+    }
+
+
+    let path = PathBuf::from(&args[1]);
+    if let Ok(_) = fs::File::open(path.as_path()) {
+        println!("Successfully opened '{0}'!", &args[1]);
+    } else {
+        println!("Could not open '{0}'...", &args[1]);
+    }
+
+    Ok(())
+}
